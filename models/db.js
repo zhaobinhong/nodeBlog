4e55729ba2881f91e7c0a3c40a406a8887464df1
zhaobinhong <zhaobinhong29@163.com>
2017-01-13 08:50:58 +0800

    node bolg

---
+++ models/db.js
@@ -0,0 +1,9 @@
+/**
+ * Created by bankeys-01 on 2017/1/11.
+ */
+var settings = require('../settings'),
+    Db = require('mongodb').Db,
+    Connection = require('mongodb').Connection,
+    Server = require('mongodb').Server;
+module.exports = new Db(settings.db, new Server(settings.host, settings.port),
+    {safe: true});
---
+++ settings.js
@@ -0,0 +1,9 @@
+/**
+ * Created by bankeys-01 on 2017/1/11.
+ */
+module.exports = {
+    cookieSecret: 'myblog',
+    db: 'blog',
+    host: 'localhost',
+    port: 27017
+};
